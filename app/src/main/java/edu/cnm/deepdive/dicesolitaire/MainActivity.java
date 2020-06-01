91b8ee62d3878995904ca1ea4a961565fd24377f
levisanchez <levijames@gmail.com>
2020-06-01 12:04:22 -0600

    Preparing to display dice images.

--- app/src/main/java/edu/cnm/deepdive/dicesolitaire/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/dicesolitaire/MainActivity.java
@@ -1,13 +1,15 @@
 package edu.cnm.deepdive.dicesolitaire;
 
+import static edu.cnm.deepdive.dicesolitaire.model.Roll.NUM_FACES;
+
 import android.content.res.Resources;
+import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 import edu.cnm.deepdive.dicesolitaire.model.Roll;
 import java.text.NumberFormat;
 import java.util.Arrays;
@@ -21,39 +23,63 @@ public class MainActivity extends AppCompatActivity {
   private static final String SCRATCH_COUNT_ID_FORMAT = "scratch_%d_count";
 
   private int minPairValue = 2;
-  private int maxPairValue;
+  private int maxPairValue = 2 * NUM_FACES;
   private TextView[] pairLabels;
   private ProgressBar[] pairCounts;
-  private Button roller;
-  private TextView rollDisplay;
-  private Random rng;
   private TextView[] scratchLabels;
   private ProgressBar[] scratchCounts;
+  private Button roller;
+  private Random rng = new Random();
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    setupUI();
+  }
+
+  private void setupUI() {
     setContentView(R.layout.activity_main);
-    maxPairValue = 2 * Roll.NUM_FACES;
+    Resources res = getResources();
+    NumberFormat formatter = NumberFormat.getInstance();
+    setupPairControls(res, formatter);
+    setupPlayControls();
+    setupScratchControls(res, formatter);
+  }
+
+  private void setupScratchControls(Resources res, NumberFormat formatter) {
+    scratchLabels = new TextView[NUM_FACES];
+    scratchCounts = new ProgressBar[NUM_FACES];
+    for (int i = 1; i <= NUM_FACES; i++) {
+      String labelIdString = String.format(SCRATCH_LABEL_ID_FORMAT, i);
+      int labelId = res.getIdentifier(labelIdString, "id", getPackageName());
+      scratchLabels[i - 1] = findViewById(labelId);
+      scratchLabels[i - 1].setText(formatter.format(i));
+      String countIdString = String.format(SCRATCH_COUNT_ID_FORMAT, i);
+      int countId = res.getIdentifier(countIdString, "id", getPackageName());
+      scratchCounts[i - 1] = findViewById(countId);
+      scratchCounts[i - 1].setProgress(1 + rng.nextInt(7));
+    }
+  }
+
+  private void setupPlayControls() {
+    roller = findViewById(R.id.roller);
+    //TODO Find and wire up dice ImageView objects
+    roller.setOnClickListener(new RollerListener());
+  }
+
+  private void setupPairControls(Resources res, NumberFormat formatter) {
     pairLabels = new TextView[maxPairValue - minPairValue + 1];
     pairCounts = new ProgressBar[maxPairValue - minPairValue + 1];
-    Resources res = getResources();
-    rng = new Random();
-    NumberFormat formatter = NumberFormat.getInstance();
-    for (int i = minPairValue; i <= maxPairValue; i++){
-      String labelIDString = String.format(PAIR_LABEL_ID_FORMAT, i);
-      int labelid = res.getIdentifier(labelIDString, "id", getPackageName());
-      pairLabels[i - minPairValue] = findViewById(labelid);
+    for (int i = minPairValue; i <= maxPairValue; i++) {
+      String labelIdString = String.format(PAIR_LABEL_ID_FORMAT, i);
+      int labelId = res.getIdentifier(labelIdString, "id", getPackageName());
+      pairLabels[i - minPairValue] = findViewById(labelId);
       pairLabels[i - minPairValue].setText(formatter.format(i));
       String countIdString = String.format(PAIR_COUNT_ID_FORMAT, i);
       int countId = res.getIdentifier(countIdString, "id", getPackageName());
       pairCounts[i - minPairValue] = findViewById(countId);
       pairCounts[i - minPairValue].setProgress(1 + rng.nextInt(10));
     }
-
-    roller = findViewById(R.id.roller);
-    rollDisplay = findViewById(R.id.roll_display);
-    roller.setOnClickListener(new RollerListener());
   }
 
   private class RollerListener implements OnClickListener {
@@ -61,9 +87,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onClick(View v) {
       Roll roll = new Roll(rng);
-      rollDisplay.setText(Arrays.toString(roll.getDice()));
-
+//TODO diplay dice images.
     }
+
   }
-//TODO add end of hw requests
+
 }
