c5f967c1c2a7d65b9418e3219a655cb2383693b0
levisanchez <levijames@gmail.com>
2020-06-04 16:58:37 -0600

    Replaced anonymous with lamdas

--- app/src/main/java/edu/cnm/deepdive/dicesolitaire/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/dicesolitaire/MainActivity.java
@@ -85,12 +85,9 @@ public class MainActivity extends AppCompatActivity {
       diceFaces[i] = getDrawable(id);
 
     }
-    roller.setOnClickListener(new OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        roller.setEnabled(false);
-        new DiceAnimator().start();
-      }
+    roller.setOnClickListener(v -> {
+      roller.setEnabled(false);
+      new DiceAnimator().start();
     });
   }
 
@@ -132,23 +129,12 @@ public class MainActivity extends AppCompatActivity {
         final int value = roll.getDice()[i];
         displayFace(dieIndex, value);
       }
-      runOnUiThread(new Runnable() {
-        @Override
-        public void run() {
-          roller.setEnabled(true);
-
-        }
-      });
+      runOnUiThread(() -> roller.setEnabled(true));
 
     }
 
     private void displayFace(final int dieIndex, final int value) {
-      runOnUiThread(new Runnable() {
-        @Override
-        public void run() {
-        displayDiceFace(dieIndex, value - 1);
-        }
-      });
+      runOnUiThread(() -> displayDiceFace(dieIndex, value - 1));
     }
   }
 
